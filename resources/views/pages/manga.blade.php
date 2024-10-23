87cb065de17f2a42b39d1e9c5b561dac7c6f9dc1
GiorgioDordo <djole6789@gmail.com>
2024-10-23 17:34:22 +0200

    adding datas to the page

--- resources/views/pages/manga.blade.php
+++ resources/views/pages/manga.blade.php
@@ -1,5 +1,4 @@
 @extends("layouts.app")
-@extends("layouts.app")
 
 @section("page-title", "Manga")
 
@@ -7,21 +6,35 @@
 <section class="container">
     <div class="row">
         <div class="col-12">
-            <h1>
+            <h1 class="text-center">
                 MANGAS
             </h1>
         </div>
-        <div class="row">
-            <div class="col-4">
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+        <div class="row d-flex justify-content-center">
+            @foreach ($mangas as $manga)
+            <div class="col-3 card m-3">
+                <ul class="list-unstyled">
+                    <li>
+                        <img src="{{$manga["immagine"]}}" alt="">
+                    </li>
+                    <li>
+                        <strong>Titolo:</strong> {{$manga["titolo"]}}
+                    </li>
+                    <li>
+                        <strong>Autore:</strong> {{$manga["autore"]}}
+                    </li>
+                    <li>
+                        <strong>Genere:</strong> {{$manga["genere"]}}
+                    </li>
+                    <li>
+                        <strong>Editore:</strong> {{$manga["editore"]}}
+                    </li>
+                    <li>
+                        <strong>Prezzo:</strong> {{$manga["prezzo"]}} <strong>€</strong>
+                    </li>
                 </ul>
             </div>
+            @endforeach
         </div>
     </div>
 </section>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('pages.home');
+});
+
+Route::get('/manga', function () {
+    $mangas = config("db.manga");
+    return view('pages.manga', compact("mangas"));
 });
